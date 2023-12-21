a3b504da8ad243194c0ae0b99dac2db01a2bdcd8
Amenallah28 <amenallahmrabet9@gmail.com>
2023-12-21 13:40:44 +0100

    builtin func

---
+++ builtin.c
@@ -0,0 +1,34 @@
+#include "shell.h"
+
+int is_built_in(char *input)
+
+{
+	char *built[] = {"exit", "env", NULL};
+	int i;
+	for (i = 0; built[i]; i++)
+	{
+		if (strcmp(input, built[i]) == 0)
+		{
+			return (1);
+		}
+	}
+	return (0);
+}
+void handle_built_in(char **input, int *status)
+{
+	int i;
+	if (strcmp(input[0], "exit") == 0)
+	{
+		freearray(input);
+		exit(*status);
+	}
+	else if (strcmp(input[0], "env") == 0)
+	{
+		for (i = 0; environ[i]; i++)
+		{
+			printf("%s\n", environ[i]);
+		}
+		freearray(input);
+		(*status) = 0;
+	}
+}
