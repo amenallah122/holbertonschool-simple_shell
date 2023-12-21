1eb8ce6f85f5004c3a7874b4ba619fbd35864b12
amenallah122 <garmaiamenallah@gmail.com>
2023-12-21 14:00:44 +0100

    execute.c

--- execute.c
+++ execute.c
@@ -1,33 +1,40 @@
 #include "shell.h"
+/**
+ * execute - executes the command
+ * @input: input
+ * @argv: argument vector
+ * @idx: index
+ * Return: status
+*/
 int execute(char **input, char **argv, int idx)
 {
-    pid_t child;
-    int status;
-    char *command;
+	pid_t child;
+	int status;
+	char *command;
 
-    command = _getpath(input[0]);
-    if (!command)
-    {
-        fprintf(stderr, "%s: %d: %s: not found\n", argv[0], idx, input[0]);
-        freearray(input);
-        return (127);
-    }
-    child = fork();
-    if (child == 0)
-    {
-        if (execve(command, input, environ) == -1)
-        {
-            perror(argv[0]);
-            freearray(input);
-            free(command);
-            exit(100);
-        }
-    }
-    else
-    {
-        waitpid(child, &status, 0);
-        freearray(input);
-        free(command);
-    }
-    return (WEXITSTATUS(status));
+	command = _getpath(input[0]);
+	if (!command)
+	{
+		fprintf(stderr, "%s: %d: %s: not found\n", argv[0], idx, input[0]);
+		freearray(input);
+		return (127);
+	}
+	child = fork();
+	if (child == 0)
+	{
+		if (execve(command, input, environ) == -1)
+		{
+			perror(argv[0]);
+			freearray(input);
+			free(command);
+			exit(100);
+		}
+	}
+	else
+	{
+		waitpid(child, &status, 0);
+		freearray(input);
+		free(command);
+	}
+	return (WEXITSTATUS(status));
 }
