f679559d2aa2a4365fc95327d6809400316eb194
amenallah122 <garmaiamenallah@gmail.com>
2023-12-21 19:10:21 +0100

    path.c

--- path.c
+++ path.c
@@ -1,30 +1,35 @@
 #include "shell.h"
+/**
+ * _getpath - function that gets the path
+ * @input: command to get the path of
+ * Return: 0
+*/
 char *_getpath(char *input)
 {
 	char *env, *path, *command;
 	struct stat st;
-    int i;
+	int i;
 
-    for (i = 0; input[i]; i++)
+	for (i = 0; input[i]; i++)
 	{
 		if (input[i] == '/')
 		{
 			if (stat(input, &st) == 0)
-				return strdup(input);
+				return (strdup(input));
 
-			return NULL;
+			return (NULL);
 		}
 	}
 
 	env = _getenv("PATH");
-	if (env == NULL)
-		return NULL;
+	if (env == (NULL))
+		return (NULL);
 
 	path = strtok(env, ":");
 	while (path)
 	{
 		command = malloc(strlen(path) + strlen(input) + 2);
-		if (command != NULL)
+		if (command != (NULL))
 		{
 			snprintf(command, strlen(path) + strlen(input) + 2, "%s/%s", path, input);
 			if (stat(command, &st) == 0)
@@ -35,15 +40,15 @@ char *_getpath(char *input)
 
 			free(command);
 
-			path = strtok(NULL, ":");
+			path = strtok((NULL), ":");
 		}
 		else
 		{
 			free(env);
-			return NULL;
+			return (NULL);
 		}
 	}
 
 	free(env);
-	return NULL;
+	return (NULL);
 }
