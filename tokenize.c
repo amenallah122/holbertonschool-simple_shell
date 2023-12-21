b701de617b01f21b63fc58fdd61d962f2fce7f38
Amenallah28 <amenallahmrabet9@gmail.com>
2023-12-21 09:54:04 +0100

    splitiing the cmds

--- tokenize.c
+++ tokenize.c
@@ -1,65 +1,44 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include "main.h"
 
-#define DELIM " " 
 char **tokenizer(char *line)
 {
 	char *token = NULL;
+	char **commands = NULL;
 	char *line_copy = NULL;
-	char **command = NULL;
-	int argc = 0, i = 0;
+	int argc = 0;
+	int i = 0;
 
-	if (line == NULL)
-		return NULL;
-
+	if (!line)
+		return (NULL);
 	line_copy = strdup(line);
 	token = strtok(line_copy, DELIM);
-
 	if (token == NULL)
 	{
-		free(line_copy);
-		line_copy = NULL;
-		return NULL;
+		free(line_copy), line_copy = NULL;
+		free(line), line = NULL;
+		return (NULL);
 	}
-
 	while (token)
 	{
 		argc++;
 		token = strtok(NULL, DELIM);
 	}
-	free(line_copy);
-	line_copy = NULL;
-
-	command = malloc(sizeof(char *) * (argc + 1));
-
-	if (command == NULL)
+	free(line_copy	), line_copy = NULL;
+	commands = malloc(sizeof(char *) * (argc + 1));
+	if (!commands)
 	{
-		free(line);
-		line = NULL;
-		return NULL;
+		free(line), line = NULL;
+		return (NULL);
 	}
-
 	token = strtok(line, DELIM);
 	while (token)
 	{
-		command[i] = strdup(token);
+		commands[i] = strdup(token);
 		token = strtok(NULL, DELIM);
 		i++;
 	}
-
-	free(line);
-	line = NULL;
-	command[i] = NULL;
-
-	return command;
+	free(line), line = NULL;
+	commands[i] = NULL;
+	return (commands);
 }
 
-int main(void)
-{
-	char *test = "Hello I am Chaima";
-	char **commands;
-	
-	commands = tokenizer(test);
-	return 1;
-}
