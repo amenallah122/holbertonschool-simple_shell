5d662ecf81190df61632a0115d455ace35fe5104
Amenallah28 <amenallahmrabet9@gmail.com>
2023-12-21 09:38:24 +0100

    header file

---
+++ main.h
@@ -0,0 +1,22 @@
+#ifndef AHLA_NEES
+#define AHLA_NEES
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <fcntl.h>
+
+#define DELIM " \t\n"
+extern char **environ;
+
+char *read_line(void);
+char **tokenizer(char *line);
+void freearray(char **array);
+int execute(char **input, char**argv);
+
+
+#endif
